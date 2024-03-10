7885c1bd1932a08e2552911e9d76116bddb5b8b0
Awadh Kishor Singh <awadhkishorsingh241@gmail.com>
2024-03-10 13:44:21 +0530

    User and Manager Work Done

--- Python/automated_library_management_system.py
+++ Python/automated_library_management_system.py
@@ -1,26 +1,24 @@
-    
+book_list = [[101,"Think and Grow Rich", "Napolean Hill",5],[102,"Making India Awesome",'Chetan Bhagat',10],[103,'A bunch of old letters','Jawahar Lal Nehru',5],[104,'Ananadath','Bankim Chandra Chatterjee',7]]
+print(f"This is the list of {len(book_list)} books already present in the library")
+print()
+
+for i in book_list:
+    print(i)
+print()
+        
 print("For Manager of Library type --> Manager in any case : ")
 print("As a user of Library Books Type your Name : ")
 
 def add_books():
-    global book_list
-    book_list = [[101,"Think and Grow Rich", "Napolean Hill",5],[102,"Making India Awesome",'Chetan Bhagat',10],[103,'A bunch of old letters','Jawahar Lal Nehru',5],[104,'Ananadath','Bankim Chandra Chatterjee',7]]
-    
-    print(f"This is the list of {len(book_list)} books already present in the library")
-    print()
-
-    for i in book_list:
-        print(i)
-    print()
+    print("Manager can add only one book at a time....")
     book_id = int(input("Enter Book ID : "))
     book_name = input("Enter Book name : ")
     book_writer = input("Enter name of the book writer : ")
     book_quantity = int(input("Enter the qunatity of BOOKS : "))
     new_book = [book_id,book_name,book_writer,book_quantity]
-    book_list = book_list.append(new_book)
+    book_list.append(new_book)
     print("The set of books available in the library currently is : \n")
     print()
-    print(book_list)
     for j in book_list:
         print(j)
     print()
@@ -38,15 +36,64 @@ def  who_are_you():
         input_by_manager = input_by_manager.lower()
         if(input_by_manager=='yes'):
             add_books()
-        
-
+        else:
+            print("Call the reader to read book ----- ")
+            who_are_you()
 
     else:
         global user
         who_are_you_input = who_are_you_input.capitalize()
         user = who_are_you_input
-        print("You are using the Library as Book reader.")
+        print("You are using the Library as Book reader.\nList of book is as folow in the library :\n")
+        
+        print(f"Your name is {user} and you are here to use Library Books") 
+        print("Basic Instructions Regarding Library Books :\n3 Books are allowed per user.\nBook taken by the reader from the library need to be submitted within 14 days.\n")
+        for j in book_list:
+            print(j)
+        print()
+#         book_taken_by_user()
+
+
+# def book_taken_by_user():
+#     book_taken_count = 0
+#     flag = 1
+#     number_of_books_that_user_want = int(input("Enter The number of books that you want : "))
+#     if(number_of_books_that_user_want<=3 and number_of_books_that_user_want>=1):
+#         for j in range(number_of_books_that_user_want):
+#             for i in range(len(book_list)):
+#                 book_id_taken = int(input("Enter The book id : "))
+#                 if(book_id_taken == book_list[i][1] and book_list[i][3]>0):
+#                     print("Congratulation your book is available and you can take it.")
+#                     flag = 0
+#                     book_taken_count+=1
+#                     book_list[i][3]-=1
+#                 if(flag==0):
+#                     break
+
+#                 else:
+#                     if(book_id_taken == book_list[i][1]):
+#                         print("Sorry, Quantity of this book Id is 0")
+#                         for j in book_list:
+#                             print(j)
+#                         print()
+#                         book_taken_by_user()
+
+#                     else:
+#                         print("Sorry, No Book found !\nTry Aagain")
+#                         for j in book_list:
+#                             print(j)
+#                         print()
+#                         book_taken_by_user()
+#     else:
+#         print("Sorry Only 3 books are allowed for a user ... ")
+#         book_taken_by_user()
+        
+            
+            
+
+
 
 who_are_you()
-# print(f"Your name is {user} and you are here to use Library Books")
-# print("Basic Instructions Regarding Library Books :\n3Books are allowed per user.\nBook taken by the reader from the library need to be submitted within 14 days.")
+
+
+
---
+++ Python/main.py
@@ -0,0 +1,7 @@
+a = [1,2,3]
+def append_int():
+    b = int(input('Enter a number : '))
+    a = a.append(b)
+    print(a)
+
+append_int()
